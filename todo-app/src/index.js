30b5e058a808bc6a0b51b4800d02de59e5bc235d
Rikin Parekh <parekh.r@northeastern.edu>
2022-04-20 12:06:19 -0400

    Update index.js

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// In React, a “root” is a pointer to the top-level data structure that React uses to track a tree to render. In the legacy API, the root was opaque to the user because we attached it to the DOM element, and accessed it through the DOM node, never exposing it to the user
 ReactDOM.render(
 
   <App />
